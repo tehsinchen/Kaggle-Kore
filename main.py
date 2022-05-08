6ca2a6e570f3dfdd0f99fad0d9cc054021fa5522
tehsin <tehsinchen@Tes-MacBook-Pro.local>
2022-05-08 15:52:03 +0800

    Set up kaggle environment

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-msg = "Hellow world"
-print(msg)
+# from kaggle_environments import make
+
+# env = make("kore_fleets", debug=True)
+
+# env.run(["./do_nothing.py"])
+# out = env.render(mode="ansi")
+# print(type(out))
+# print(out)
+
+from kaggle_environments import make
+
+# Inflate the response replay to visualize.
+env = make("kore_fleets", debug=True)
+env.run(["./do_nothing.py"])
+env.render(mode="ipython")
