f17650834c75e14a81bba47a8c72e89a80372fc2
tehsin <tehsinchen@Tes-MacBook-Pro.local>
2022-05-14 13:48:21 +0800

    Add Jupyter file into git control

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-# from kaggle_environments import make
-
-# env = make("kore_fleets", debug=True)
-
-# env.run(["./do_nothing.py"])
-# out = env.render(mode="ansi")
-# print(type(out))
-# print(out)
-
 from kaggle_environments import make
 
-# Inflate the response replay to visualize.
 env = make("kore_fleets", debug=True)
+
 env.run(["./do_nothing.py"])
-env.render(mode="ipython")
+out = env.render(mode="ansi")
+print(type(out))
+print(out)
+
+# from kaggle_environments import make
+
+# Inflate the response replay to visualize.
+# env = make("kore_fleets", debug=True)
+# env.run(["./do_nothing.py"])
+# env.render(mode="ipython")
