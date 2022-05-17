677b9a4a0204b20b62a699f93f0c0b28c299880e
unknown <tteess1998@gmail.com>
2022-05-17 20:31:25 +0800

    add tess file into git control

---
+++ pilot_tess.py
@@ -0,0 +1,10 @@
+from kaggle_environments.envs.kore_fleets.helpers import Board
+
+from messanger import get_info
+from headquarter_tess import HeadQuarter
+
+
+def pilot(obs, config):
+    message = get_info(Board(obs, config))
+    hq = HeadQuarter(message)
+    return hq.get_command()
